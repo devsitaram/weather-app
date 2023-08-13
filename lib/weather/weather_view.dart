2d41410666d858be5ead9be0500bfc3740487cb7
Sita Ram Thing <sitaramthing667@gmail.com>
2023-08-13 19:24:15 +0545

    create the new weather class and get the data

--- lib/weather/weather_view.dart
+++ lib/weather/weather_view.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:weather/weather/weather_model.dart';
 import 'package:weather/weather/weather_services.dart';
 
 class WeatherView extends StatefulWidget {
@@ -10,12 +14,41 @@ class WeatherView extends StatefulWidget {
 }
 
 class _WeatherViewState extends State<WeatherView> {
+  final formKey = GlobalKey<FormState>();
+  final myController = TextEditingController();
+  final storage = const FlutterSecureStorage();
+
+  WeatherModel?weather; // to hold the retrieved weather data
+
+  @override
+  void initState() {
+    super.initState();
+    fetchStoredWeatherData(); // Fetch the stored weather data when the widget initializes
+  }
+
+  Future<void> fetchStoredWeatherData() async {
+    final weatherDataString = await storage.read(key: "weatherData");
+    if (weatherDataString != null) {
+      final decodedData = jsonDecode(weatherDataString);
+      setState(() {
+        weather = WeatherModel.fromJson(decodedData);
+      });
+    }
+  }
+
+  // show the toast message
+  void toastMessage(String smg) {
+    Fluttertoast.showToast(
+        msg: smg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // create the local variable
-    final formKey = GlobalKey<FormState>();
-    final myController = TextEditingController();
-
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -27,17 +60,21 @@ class _WeatherViewState extends State<WeatherView> {
                 // text fields
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'The field is empty!';
-                      }
-                      return null;
-                    },
-                    controller: myController,
-                    decoration: const InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 0.5, color: Colors.black),
+                  child: Form(
+                    key: formKey,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Enter the city name!';
+                        }
+                        return null;
+                      },
+                      controller: myController,
+                      decoration: const InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 0.5, color: Colors.black),
+                        ),
                       ),
                     ),
                   ),
@@ -45,11 +82,23 @@ class _WeatherViewState extends State<WeatherView> {
 
                 // text button
                 TextButton(
-                  onPressed: () {
+                  onPressed: () async {
                     if (formKey.currentState!.validate()) {
-                      // print(myController.text);
-                      const FlutterSecureStorage()
-                          .write(key: "value", value: myController.text);
+                      var newWeather =
+                          await WeatherService.getWeather(myController.text);
+                      if (newWeather == null) {
+                        toastMessage("Weather data not available!");
+                      } else {
+                        final weatherDataString = jsonEncode(newWeather);
+                        await storage.write(
+                            key: "weatherData", value: weatherDataString);
+                        setState(
+                          () {
+                            weather = newWeather;
+                          },
+                        );
+                        toastMessage("Success");
+                      }
                     }
                   },
                   child: const Text(
@@ -58,14 +107,9 @@ class _WeatherViewState extends State<WeatherView> {
                   ),
                 ),
               ],
-            )
-
-            // TextButton(
-            //   onPressed: () {
-            //     WeatherService.getWeather("Kathmandu");
-            //   },
-            //   child: const Text("Get Weather"),
-            // ),
+            ),
+            if (weather != null) // Display weather data if available
+              Text("Temperature: ${weather!.main?.temp}"),
           ],
         ),
       ),
